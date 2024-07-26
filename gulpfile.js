914ef91c5615d2ca1a54f033698edc65cb98c93c
Dunaev Vladimir <dunaevvk@gmail.com>
2024-07-26 13:43:11 +0700

    font

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, watch, series, parallel} = require("gulp")
+const { src, dest, watch, series, parallel } = require("gulp")
 const del = require("del")
 const sass = require("gulp-sass")(require("sass"))
 const rename = require("gulp-rename")
@@ -10,78 +10,95 @@ const autoprefixer = require("gulp-autoprefixer")
 const pug = require("gulp-pug")
 const concat = require("gulp-concat")
 const babel = require("gulp-babel")
+const fonter = require("gulp-fonter")
+const ttf2woff2 = require("gulp-ttf2woff2")
 const uglify = require("gulp-uglify")
 const sourcemaps = require("gulp-sourcemaps")
 const insert = require("gulp-insert")
 const replace = require("gulp-replace")
 const include = require("gulp-include")
-const {execSync} = require("child_process")
+const { execSync } = require("child_process")
 
 // Task functions for development mode
 const setEnv = (cb) => {
-    process.env.NODE_ENV = "dev"
-    cb()
+	process.env.NODE_ENV = "dev"
+	cb()
 }
+const fonts = () => src('./src/fonts/src/*.*')
+	.pipe(fonter({
+		formats: ['woff', 'ttf']
+	}))
+	.pipe(src('./src/fonts/*.ttf'))
+	.pipe(ttf2woff2())
+	.pipe(dest("./src/fonts"))
 
 const devVendorCss = () => src("./src/vendor/scss/**/*.scss")
-	.pipe(plumber({errorHandler: notify.onError(err => ({
-		title: "Scss styles Error into the devVendorCss",
-		message: err.message
-	}))}))
+	.pipe(plumber({
+		errorHandler: notify.onError(err => ({
+			title: "Scss styles Error into the devVendorCss",
+			message: err.message
+		}))
+	}))
 	.pipe(sourcemaps.init())
 	.pipe(sass.sync().on("error", sass.logError))
 	.pipe(autoprefixer({
-			overrideBrowserslist: ["last 2 versions"],
-			cascade: false
-		}))
-	.pipe(rename({basename: "vendor"}))
+		overrideBrowserslist: ["last 2 versions"],
+		cascade: false
+	}))
+	.pipe(rename({ basename: "vendor" }))
 	.pipe(sourcemaps.write())
 	.pipe(plumber.stop())
 	.pipe(dest("build/css"))
 	.pipe(browserSync.stream())
 
 const devCommonCss = () => src("./src/scss/**/*.scss")
-	.pipe(plumber({errorHandler: notify.onError(err => ({
+	.pipe(plumber({
+		errorHandler: notify.onError(err => ({
 			title: "Scss styles Error into the devCommonCss",
 			message: err.message
-		}))}))
+		}))
+	}))
 	.pipe(sourcemaps.init())
 	.pipe(sass.sync().on("error", sass.logError))
 	.pipe(autoprefixer({
-			overrideBrowserslist: ["last 2 versions"],
-			cascade: false
-		}))
-	.pipe(rename({basename: "main"}))
+		overrideBrowserslist: ["last 2 versions"],
+		cascade: false
+	}))
+	.pipe(rename({ basename: "main" }))
 	.pipe(sourcemaps.write())
 	.pipe(plumber.stop())
-	.pipe(dest(	"build/css"))
+	.pipe(dest("build/css"))
 	.pipe(browserSync.stream())
 
 const devCustomCss = () => src("./src/pages/**/style.scss")
-	.pipe(plumber({errorHandler: notify.onError(err => ({
+	.pipe(plumber({
+		errorHandler: notify.onError(err => ({
 			title: "Scss styles Error into the devCustomCss",
 			message: err.message
-		}))}))
+		}))
+	}))
 	.pipe(sourcemaps.init())
 	.pipe(sass.sync().on("error", sass.logError))
 	.pipe(autoprefixer({
-			overrideBrowserslist: ["last 2 versions"],
-			cascade: false
-		}))
+		overrideBrowserslist: ["last 2 versions"],
+		cascade: false
+	}))
 	.pipe(rename(fileObj => {
-			fileObj.basename = fileObj.dirname
-			fileObj.dirname = ""
-		}))
+		fileObj.basename = fileObj.dirname
+		fileObj.dirname = ""
+	}))
 	.pipe(sourcemaps.write())
 	.pipe(plumber.stop())
 	.pipe(dest("build/css/pages"))
 	.pipe(browserSync.stream())
 
 const devVendorJs = () => src("./src/vendor/js/**/*.js")
-	.pipe(plumber({errorHandler: notify.onError(err => ({
+	.pipe(plumber({
+		errorHandler: notify.onError(err => ({
 			title: "JS Error into the devVendorJs",
 			message: err.message
-		}))}))
+		}))
+	}))
 	.pipe(sourcemaps.init())
 	.pipe(concat("vendor.js"))
 	.pipe(sourcemaps.write())
@@ -90,16 +107,18 @@ const devVendorJs = () => src("./src/vendor/js/**/*.js")
 	.pipe(browserSync.stream())
 
 const devCommonJs = () => src([
-		"./src/js/utils/**/*.js",
-		"./src/components/**/*.js",
-		"./src/template/**/*.js",
-		"./src/js/main.js",
-		"./src/js/**/*.js"
-	])
-	.pipe(plumber({errorHandler: notify.onError(err => ({
+	"./src/js/utils/**/*.js",
+	"./src/components/**/*.js",
+	"./src/template/**/*.js",
+	"./src/js/main.js",
+	"./src/js/**/*.js"
+])
+	.pipe(plumber({
+		errorHandler: notify.onError(err => ({
 			title: "JS Error into the devCommonJs",
 			message: err.message
-		}))}))
+		}))
+	}))
 	.pipe(sourcemaps.init())
 	.pipe(concat("main.js"))
 	.pipe(insert.prepend("'use strict';\n\n"))
@@ -109,16 +128,18 @@ const devCommonJs = () => src([
 	.pipe(browserSync.stream())
 
 const devCustomJs = () => src("./src/pages/**/script.js")
-	.pipe(plumber({errorHandler: notify.onError(err => ({
+	.pipe(plumber({
+		errorHandler: notify.onError(err => ({
 			title: "JS Error into the devCustomJs",
 			message: err.message
-		}))}))
+		}))
+	}))
 	.pipe(sourcemaps.init())
 	.pipe(include()).on("error", console.log)
 	.pipe(rename(fileObj => {
-			fileObj.basename = fileObj.dirname
-			fileObj.dirname = ""
-		}))
+		fileObj.basename = fileObj.dirname
+		fileObj.dirname = ""
+	}))
 	// .pipe(insert.prepend("'use strict';\n\n"))
 	.pipe(sourcemaps.write())
 	.pipe(plumber.stop())
@@ -126,16 +147,18 @@ const devCustomJs = () => src("./src/pages/**/script.js")
 	.pipe(browserSync.stream())
 
 const devHtml = () => src("./src/pages/**/index.pug")
-	.pipe(plumber({errorHandler: notify.onError(err => ({
+	.pipe(plumber({
+		errorHandler: notify.onError(err => ({
 			title: "Pug Error into the devHtml",
 			message: err.message
-		}))}))
-	.pipe(pug({pretty: true}))
+		}))
+	}))
+	.pipe(pug({ pretty: true }))
 	.pipe(replace(/\/>/g, ">"))
 	.pipe(rename(fileObj => {
-			fileObj.basename = fileObj.dirname
-			fileObj.dirname = ""
-		}))
+		fileObj.basename = fileObj.dirname
+		fileObj.dirname = ""
+	}))
 	.pipe(plumber.stop())
 	.pipe(dest("build"))
 	.pipe(browserSync.stream())
@@ -143,88 +166,88 @@ const devHtml = () => src("./src/pages/**/index.pug")
 // Task functions for build mode
 const buildVendorCss = () => src("./src/vendor/scss/**/*.scss")
 	.pipe(sass.sync().on("error", sass.logError))
-	.pipe(mmq({log: true, skipMQErrors: true}))
+	.pipe(mmq({ log: true, skipMQErrors: true }))
 	.pipe(autoprefixer({
-			overrideBrowserslist: ["last 2 versions"],
-			cascade: false
-		}))
-	.pipe(sass.sync({outputStyle: "compressed"}))
-	.pipe(rename({basename: "vendor", suffix: ".min"}))
+		overrideBrowserslist: ["last 2 versions"],
+		cascade: false
+	}))
+	.pipe(sass.sync({ outputStyle: "compressed" }))
+	.pipe(rename({ basename: "vendor", suffix: ".min" }))
 	.pipe(dest("build/css"))
 
 const buildCommonCss = () => src("./src/scss/**/*.scss")
 	.pipe(sass.sync().on("error", sass.logError))
 	// .pipe(mmq({log: true, skipMQErrors: true}))
 	.pipe(autoprefixer({
-			overrideBrowserslist: ["last 2 versions"],
-			cascade: false
-		}))
-	.pipe(sass.sync({outputStyle: "compressed"}))
-	.pipe(rename({basename: "main", suffix: ".min"}))
+		overrideBrowserslist: ["last 2 versions"],
+		cascade: false
+	}))
+	.pipe(sass.sync({ outputStyle: "compressed" }))
+	.pipe(rename({ basename: "main", suffix: ".min" }))
 	.pipe(dest("build/css"))
 
 const buildCustomCss = () => src("./src/pages/**/style.scss")
 	.pipe(sass.sync().on("error", sass.logError))
 	// .pipe(mmq({log: true, skipMQErrors: true}))
 	.pipe(autoprefixer({
-			overrideBrowserslist: ["last 2 versions"],
-			cascade: false
-		}))
-	.pipe(sass.sync({outputStyle: "compressed"}))
+		overrideBrowserslist: ["last 2 versions"],
+		cascade: false
+	}))
+	.pipe(sass.sync({ outputStyle: "compressed" }))
 	.pipe(rename(fileObj => {
-			fileObj.basename = fileObj.dirname + ".min"
-			fileObj.dirname = ""
-		}))
+		fileObj.basename = fileObj.dirname + ".min"
+		fileObj.dirname = ""
+	}))
 	.pipe(dest("build/css/pages"))
 
 const buildVendorJs = () => src("./src/vendor/js/**/*.js")
 	.pipe(concat("vendor.js"))
 	// .pipe(babel({presets: [['@babel/preset-env']]}))
 	.pipe(uglify())
-	.pipe(rename({suffix: ".min"}))
+	.pipe(rename({ suffix: ".min" }))
 	.pipe(dest("build/js"))
 
 const buildCommonJs = () => src([
-		"./src/js/utils/**/*.js",
-		"./src/components/**/*.js",
-		"./src/template/**/*.js",
-		"./src/js/main.js",
-		"./src/js/**/*.js",
-	])
+	"./src/js/utils/**/*.js",
+	"./src/components/**/*.js",
+	"./src/template/**/*.js",
+	"./src/js/main.js",
+	"./src/js/**/*.js",
+])
 	.pipe(concat("main.js"))
-	.pipe(babel({compact: false, presets: [["@babel/preset-env"]]}))
+	.pipe(babel({ compact: false, presets: [["@babel/preset-env"]] }))
 	.pipe(uglify())
-	.pipe(rename({suffix: ".min"}))
+	.pipe(rename({ suffix: ".min" }))
 	.pipe(dest("build/js"))
 
 const buildCustomJs = () => src("./src/pages/**/script.js")
 	.pipe(include()).on("error", console.log)
-	.pipe(babel({compact: false, presets: [["@babel/preset-env"]]}))
+	.pipe(babel({ compact: false, presets: [["@babel/preset-env"]] }))
 	.pipe(uglify())
 	.pipe(rename(fileObj => {
-			fileObj.basename = fileObj.dirname + ".min"
-			fileObj.dirname = ""
-		}))
+		fileObj.basename = fileObj.dirname + ".min"
+		fileObj.dirname = ""
+	}))
 	.pipe(dest("build/js/pages"))
 
 const buildHtml = () => src("./src/pages/**/index.pug")
-	.pipe(pug({pretty: true}))
+	.pipe(pug({ pretty: true }))
 	.pipe(replace(/\/>/g, ">"))
 	.pipe(rename(fileObj => {
-			fileObj.basename = fileObj.dirname
-			fileObj.dirname = ""
-		}))
+		fileObj.basename = fileObj.dirname
+		fileObj.dirname = ""
+	}))
 	.pipe(dest("build"))
 
 // Common task functions
 const clean = () => del("./build")
 
 const server = () => {
-    browserSync.init({
-        server: {
-            baseDir: './build'
-        }
-    })
+	browserSync.init({
+		server: {
+			baseDir: './build'
+		}
+	})
 }
 
 // Watchers
@@ -240,11 +263,13 @@ watch("./src/components/**/*.js", devCommonJs)
 watch("./src/pages/**/*.js", devCustomJs)
 watch("./src/**/*.pug", devHtml)
 
+exports.fonts = fonts;
+
 // Project development mode
 exports.default = series(
-    clean,
-    setEnv,
-    parallel(
+	clean,
+	setEnv,
+	parallel(
 		devVendorCss,
 		devCommonCss,
 		devCustomCss,
@@ -252,13 +277,13 @@ exports.default = series(
 		devCommonJs,
 		devCustomJs,
 		devHtml
-    ),
-    server
+	),
+	server
 )
 
 // Project build mode
 exports.build = series(
-    clean,
+	clean,
 	parallel(
 		buildVendorCss,
 		buildCommonCss,
@@ -267,10 +292,10 @@ exports.build = series(
 		buildCommonJs,
 		buildCustomJs,
 		buildHtml
-    ),
+	),
 
 	(done) => {
 		done()
 		process.exit()
-    }
+	}
 )
---
+++ src/template/main-title/script.js
@@ -0,0 +1,10 @@
+const initFooterScripts = () => {
+    console.log('Здесь можно добавить JS для Главного заголовка');
+}
+
+window.addEventListener('load', () => {
+
+    // Добавляем сюда все скрипты для футера
+
+    initFooterScripts()
+})
