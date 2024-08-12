b64e163ce4c7bff1bd8bd6ed098318389ab1efad
Dunaev Vladimir <dunaevvk@gmail.com>
2024-08-12 12:54:23 +0700

    js, sort, filter

--- src/template/catalog-filters/script.js
+++ src/template/catalog-filters/script.js
@@ -5,17 +5,28 @@ const initCatalogFiltersScripts = () => {
 window.addEventListener('load', () => {
 
     // Добавляем сюда все скрипты для Catalog-filters
+    const customCheckbox = document.querySelectorAll('.custom-checkbox');
     const catalogFilterTop = document.querySelectorAll('.catalog-filter__top');
     const catalogFilter = document.querySelectorAll('.catalog-filter');
     const catalogFilterBtn = document.querySelector('.catalog-filters__btn').addEventListener('click', () => {
         catalogFilter.forEach(el => {
             el.closest('.catalog-filter').classList.toggle('catalog-filter--show')
+
         })
     });
     catalogFilterTop.forEach(el => {
         el.addEventListener('click', () => {
             el.closest('.catalog-filter').classList.toggle('catalog-filter--open')
+            el.querySelector('.catalog-filter__title').classList.toggle('catalog-filter__title--active')
+
         })
+    })
+    customCheckbox.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            el.classList.toggle('custom-checkbox--active')
+        })
+
     })
     initCatalogFiltersScripts()
 })
--- src/template/catalog/script.js
+++ src/template/catalog/script.js
@@ -1,10 +1,28 @@
-// const initFooterScripts = () => {
-//     console.log('Здесь можно добавить JS для футера');
-// }
+const initCatalogScripts = () => {
+    console.log('Здесь можно добавить JS для Каталога');
+}
 
-// window.addEventListener('load', () => {
+window.addEventListener('load', () => {
 
-//     // Добавляем сюда все скрипты для Breadcrumbs
+    // Добавляем сюда все скрипты для Catalog
+    const customSelect = document.querySelectorAll('.custom-select')
+    const customSelectIcon = document.querySelectorAll('.sort__select-icon')
+    customSelect.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.currentTarget.classList.toggle('custom-select--open')
+            // console.dir(e.target.parentElement);
+            // console.log(e.target.classList)
+            // console.log(e.target);
 
-//     initFooterScripts()
-// })
+            if (e.target.classList.contains('custom-select__option')) {
+                // console.log(e.currentTarget);
+                e.currentTarget.querySelector('.custom-select__value').textContent = e.target.textContent;
+            }
+            if (e.target.classList.contains('sort__select-icon')) {
+                console.log(e.currentTarget);
+            }
+        })
+
+    })
+    initCatalogScripts()
+})
