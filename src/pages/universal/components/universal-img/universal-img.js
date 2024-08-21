e9532bc8aaf606f04f37eafe1c2c56025e98034f
Dunaev Vladimir <dunaevvk@gmail.com>
2024-08-21 20:51:34 +0700

    add price range and mobile and universal page

---
+++ src/pages/universal/components/universal-img/universal-img.js
@@ -0,0 +1,10 @@
+const initUniversalScripts = () => {
+    console.log('Здесь можно добавить JS для КарточкиКаталога');
+}
+
+window.addEventListener('load', () => {
+
+    // Добавляем сюда все скрипты для Universal
+
+    initUniversalScripts()
+})
