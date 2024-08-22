d1355dce0f179165cadba3370945652369bd156e
AnarAgaev <anar.n.agaev@gamil.com>
2024-08-22 17:08:33 +0300

    Removed Vladimir's code from Menu-catalog page components

--- src/pages/catalog/components/catalog/catalog.js
+++ src/pages/catalog/components/catalog/catalog.js
@@ -142,24 +142,6 @@ function initLinkHover() {
 }
 
 window.addEventListener('load', () => {
-    // const catalogPageTop = document.querySelectorAll('.catalog-page__top');
-
-    // // catalogPageCard.addEventListener('click', (e) => {
-    // //     console.log(e.target);
-
-    // // })
-    // catalogPageTop.forEach(el => {
-    //     // el.stopPropagation();
-    //     el.addEventListener('click', (e) => {
-    //         // console.log(el);
-    //         // e.stopPropagation();
-    //         el.closest('.catalog-page__card').classList.toggle('catalog-page__card_open')
-    //         // el.querySelector('.card-filter__title').classList.toggle('card-filter__title--active')
-
-    //     })
-    // })
-    // // Добавляем сюда все скрипты для Catalog.catalog-page__card_open
-
     initToggleCatalogList()
     initLinkHover()
 })
