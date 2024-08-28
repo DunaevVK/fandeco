037443d935b1356c97dd67c9542d005a9137b8d8
Dunaev Vladimir <dunaevvk@gmail.com>
2024-08-28 07:08:45 +0700

    refactoring mob-menu

--- src/template/header/script.js
+++ src/template/header/script.js
@@ -1,26 +1,41 @@
 const initHeaderScripts = () => {
-        // Добавляем сюда все скрипты для хедера
-        const menuBurger = document.querySelector(".header-info__menu--burger")
-        const menu = document.querySelector(".nav-mobile")
+    // Добавляем сюда все скрипты для хедера
+    const menuBurger = document.querySelector(".header-info__menu--burger")
+    const menu = document.querySelector(".nav-mobile")
+    const closeMenu = document.querySelector(".nav-mobile__header_close")
+    const openSubMenu = document.querySelector(".nav-mobile__open-icon")
 
-        const cff = document.querySelectorAll(".nav-mobile__title")
-
-        const cards = document.querySelectorAll(".nav-mobile__top")
-        const bottom = document.querySelectorAll(".nav-mobile__bottom")
-
-        menuBurger.addEventListener("click", () => {
-            menuBurger.classList.toggle("header-info__menu--burger-active")
-            menu.classList.toggle("nav-mobile--display")
+    const initMenu = () => {
+        openSubMenu.classList.remove('nav-mobile__open-icon_active');
+        document.querySelector('.nav-mobile__list_drop').classList.remove('nav-mobile__list_drop-open')
+        document.querySelectorAll('.drop-submenu__list').forEach(el => {
+            el.classList.remove('drop-submenu__list_open')
         })
-
-        cards.forEach(el => {
-            el.addEventListener("click", () => {
-                el.closest('.nav-mobile__card').querySelector('.nav-mobile__open-icon').classList.toggle('nav-mobile__open-icon--active')
-                el.closest('.nav-mobile__card').querySelectorAll(".nav-mobile__bottom").forEach(e => {
-                    e.classList.toggle('nav-mobile__bottom-open')
-                })
-            })
+        document.querySelectorAll('.drop-submenu__item').forEach(el => {
+            el.classList.remove('drop-submenu__item_open')
         })
+    }
+
+    menuBurger.addEventListener("click", () => {
+        menu.classList.add("nav-mobile__open");
+        initMenu();
+    });
+
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove("nav-mobile__open");
+    })
+
+    menu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('nav-mobile__link_drop') || e.target.classList.contains('nav-mobile__open-icon')) {
+            e.preventDefault();
+            e.target.closest('.nav-mobile__list').querySelector('.nav-mobile__list_drop').classList.toggle('nav-mobile__list_drop-open');
+            openSubMenu.classList.toggle('nav-mobile__open-icon_active');
+        }
+        if (e.target.classList.contains('drop-submenu__title') || e.target.classList.contains('drop-submenu__plus')) {
+            e.target.closest('.drop-submenu__item').querySelector('.drop-submenu__list').classList.toggle('drop-submenu__list_open')
+            e.target.closest('.drop-submenu__item').classList.toggle('drop-submenu__item_open')
+        }
+    })
 }
 
 window.addEventListener("load", () => {
